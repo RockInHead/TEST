5a705c7ad0fc6f6bb1bc4a6d24038161a6c24a71
RockInHead <steeltim83@gmail.com>
2024-10-21 12:25:45 +0700

    Реализовал красивый ввод в консоль, чтобы консоль очищалась и казалась статичной

--- LAB №1/main.cpp
+++ LAB №1/main.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
-
+#include <windows.h>
 using namespace std;
 struct DynamicArray{
     //Объявление массива
-    int* currentArray= new int[4] {12,123,1,256};
+    int *currentArray= new int[4]{NULL,NULL,NULL,NULL};
+    int Capacity=4;
+    int Length;
+    // void ExpandArray(int array[]){
+    //     if(Length>Capacity){
+    //         int previousArray= *currentArray;
+    //         currentArray= new int[Length+4];
 
+    //         for(int i=0;i<Length;i++){
+    //         currentArray[i]=previousArray[i];
+    //         }
+
+    //         for(int j=Length;j<Length+4;j++){
+    //             currentArray[j]=NULL;
+    //         }
+    //         Capacity=Length+4;
+    //     }
+
+    // }
+    void AddElement(int array[]){
+        for(int i=0;i<4;i++){
+            if(array[i]==NULL){
+                cout<<"Enter a new element:";
+                cin>>array[i];
+                Length++;
+                break;
+                break;
+            }
+        }
+    }
+
+    //Функция возвращающая количество элементов в массиве
+    // int Length(int array[]){
+    //     return sizeof(array)/sizeof(array[0]);
+    // }
 
     //Функция сортировки массива по возрастанию
     void SortAscending(int array[])
@@ -16,7 +49,6 @@ struct DynamicArray{
                 int element= array[j];
                 array[j]=array[j+1];
                 array[j+1]=element;
-                // swap(array[j+1],array[j]);
             }
         }
     }
@@ -35,38 +67,83 @@ struct DynamicArray{
         }
     }
     }
-    //Функция выводящая символы массива в консоль
+    
+ }; 
+ //Функция выводящая символы массива в консоль
     void ShowArray(int array[]){
         for(int i=0;i<4;i++){
-        cout<<array[i]<<" ";
+        if(array[i]!=0){
+            cout<<array[i]<<" ";
+        }
     }
-    }
- }; 
-
+    cout<<" "<<endl;
+    };
+void Menu(int array[]){
+    cout<<"Current array:";
+    ShowArray(array);
+    cout<<"[1] - Add new Elemenet"<< endl;
+    cout<<"[2] - Sort in ascending order"<< endl;
+    cout<<"[3] - Sort in descendin order"<< endl;
+    cout<<"[4] - Show array"<< endl;
+    cout<<"[0] - Exit"<< endl;
+}
 int main(){
-    DynamicArray array;
-    
-    // for(int i=0;i<4;i++){
-    //     cout<<array.currentArray[i]<<endl;
-    // }
-    array.ShowArray(array.currentArray);
-    cout<<" "<<endl;
-    
-    array.SortAscending(array.currentArray);
-    array.ShowArray(array.currentArray);
-    cout<<" "<<endl;
-    // for(int i=0;i<4;i++){
-    //     cout<<array.currentArray[i]<<endl;
-    // }
-    
 
-    array.SortDescending(array.currentArray);
-    array.ShowArray(array.currentArray);
-    // for(int i=0;i<4;i++){
-    //     cout<<array.currentArray[i]<<endl;
-    // }
-    cout<<" "<<endl;
+
+
+    DynamicArray array;
+    int CommandNumber;
+
+    bool ProgramState=true;
+
+    while(ProgramState){
+    Menu(array.currentArray);
+    cin>>CommandNumber;
+    switch (CommandNumber)
+    {
+    case 1:
+        array.AddElement(array.currentArray);
+        system("cls");
+        // array.ShowArray(array.currentArray);
+        break;
     
+    case 2:
+        array.SortAscending(array.currentArray);
+        system("cls");
+
+        // ShowArray(array.currentArray);
+        break;
+    case 3:
+        array.SortDescending(array.currentArray);
+        system("cls");
+
+        // ShowArray(array.currentArray);
+        break;
+    case 4:
+        ShowArray(array.currentArray);
+        break;
+    case 0:
+        ProgramState=false;
+        break;
+    case 5:
+        system("cls");
+
+    }
+    }
+    // array.AddElement(array.currentArray);
+    // array.ShowArray(array.currentArray);
+    // // cout<<" "<<endl;
+    
+    // array.SortAscending(array.currentArray);
+    // array.ShowArray(array.currentArray);
+    // // cout<<" "<<endl;
+
+    // array.SortDescending(array.currentArray);
+    // array.ShowArray(array.currentArray);
+    // // cout<<" "<<endl;
+    // // int len = array.Length(array.currentArray);
+    // // cout<<len<<endl;
+    // cout<<array.Length;
 
 }
  
--- LAB №1/tempCodeRunnerFile.cpp
+++ LAB №1/tempCodeRunnerFile.cpp
@@ -1,3 +1,14 @@
-for(int i=0;i<4;i++){
-    //     cout<<array.currentArray[i]<<endl;
+void ExpandArray(int array[]){
+    //     if(Length>Capacity){
+    //         int previousArray= *currentArray;
+    //         currentArray= new int[Length+4];
+    //         for(int i=0;i<Length;i++){
+    //         currentArray[i]=previousArray[i];
+    //         }
+    //         for(int i=Length;i<Length+4;i++){
+    //             currentArray[i]=NULL;
+    //         }
+    //         Capacity=Length+4;
+    //     }
+
     // }
