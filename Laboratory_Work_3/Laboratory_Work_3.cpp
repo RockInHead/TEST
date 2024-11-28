98b20105affe9b7aba34c5409e5499e204a7f4d0
RockInHead <steeltim83@gmail.com>
2024-11-28 13:59:16 +0700

    Накидал меню с возможность выбора текущего массива. А далее уже выбор функция над Stack, пока только для него реализовал простешее меню.

--- Laboratory_Work_3/Laboratory_Work_3.cpp
+++ Laboratory_Work_3/Laboratory_Work_3.cpp
@@ -1,20 +1,117 @@
-﻿// Laboratory_Work_3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+
+//void ShowArray(List list)
+//{
+//    int* array = list.GetList();
+//    if (array)
+//        for (int i = 0; i < list.GetSize(); i++)
+//        {
+//            if (i == 0) {
+//                cout << GREEN << array[i] << RESET << " ";
+//            }
+//            else if (i == list.GetSize() - 1) {
+//                cout << RED << array[i] << RESET << " ";
+//
+//            }
+//            else {
+//                cout << array[i] << " ";
+//            }
+//        }
+//    cout << " " << endl;
+//    delete[] array;
+//};
+void Menu()
+{
+    cout << "Choose array:" << endl<< endl;
+    cout << "[1]" << " - Stack" << endl;
+    cout << "[2]" << " - Ring buffer" << endl;
+    cout << "[3]" << " - A Queue based on a ring buffer" << endl;
+    cout << "[4]" << " - A Queue based on 2 stacks" << endl;
+}
+
+void MenuStack()
+{
+    cout << "Stack array:" << endl << endl;
+    cout << "[1]" << " - Push" << endl;
+    cout << "[2]" << " - Pop" << endl;
+    cout << "[3]" << " - Clearing the memory" << endl;
+
+}
+int ValidCin()
+{
+    int input;
+    while (!(cin >> input)) {
+        cout << "Invalid input. Please enter an integer! " << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    return input;
+}
+int ValidInputMenu(int numbOfFirstCommand,int numbOfLastCommnad)
+{
+    int Input;
+    while ((!(cin >> Input)) || Input > numbOfLastCommnad || Input < numbOfFirstCommand) {
+        system("cls");
+        cout << "Invalid input. Please enter an integer! " << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    return Input;
+}
+
+void Stack() {
+    bool stackState = true;
+    int commandNumber;
+    while (stackState)
+    {
+        MenuStack();
+        int commandNumber;
+        commandNumber = ValidInputMenu(0,4);
+        switch (commandNumber)
+        {
+        case 1:
+
+            cout << "Push command" << endl;
+            break;
+        case 2:
+            cout << "Pop command" << endl;
+
+            break;
+        case 3:
+            cout << "Clear the memory" << endl;
+            break;
+        case 0:
+            system("cls");
+            stackState = false;
+            break;
+        }
+
+
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    bool programState = true;
+    int arrayType;
+    while (programState) {
+        Menu();
+        arrayType = ValidInputMenu(1,4);
+        switch (arrayType)
+        {
+        case 1:
+            system("cls");
+            Stack();
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        }
+    }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
